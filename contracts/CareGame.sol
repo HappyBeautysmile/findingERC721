3b5df5e8d32e710ffe8f319edf4e2be4d999358a
DarkBear0121 <darkbear0112@gmail.com>
2018-07-01 13:43:58 +0200

    message

---
+++ contracts/CareGame.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.24;
+import "./Ownable.sol";
+import "./Hack721Token.sol";
+// This is just a simple example of a coin-like contract.
+// It is not standards compatible and cannot be expected to talk to other
+// coin/token contracts. If you want to create a standards-compliant
+// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+contract CareGame is Ownable {
+
+    Hack721Token token;
+    uint ethAmount;
+    address receiver;
+    uint amountToUnlock;
+
+    constructor(address addr, address p_receiver, uint p_ethAmount, uint p_amountToUnlock) {
+        token = Hack721Token(addr);
+        receiver = p_receiver;
+        ethAmount = p_ethAmount;
+        amountToUnlock = p_amountToUnlock;
+    }
+
+    function checkAchievements() {
+        if (token.balanceOf(this.address) >= p_amountToUnlock) {
+            receiver.send(ethAmount);
+        }
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Hack721Token = artifacts.require("Hack721Token.sol");
+var CareGame = artifacts.require("CareGame.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Hack721Token);
+    //deployer.deploy(Hack721Token);
+    deployer.deploy(CareGame);
 };
